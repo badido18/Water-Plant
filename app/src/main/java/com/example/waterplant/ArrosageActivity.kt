f038c2e59822b2c52c44fb6f8c6d9eef8e6acca8
anesboz <anesbouzouaoui@gmail.com>
2022-01-08 23:23:55 +0100

    Sat Jan  8 23:23:55 CET 2022

---
+++ app/src/main/java/com/example/waterplant/ArrosageActivity.kt
@@ -0,0 +1,17 @@
+package com.example.waterplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.waterplant.databinding.ActivityArrosageBinding
+
+class ArrosageActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityArrosageBinding // binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding  = ActivityArrosageBinding.inflate(layoutInflater) // binding
+        setContentView(binding.root) // binding
+
+
+    }
+}
---
+++ app/src/main/java/com/example/waterplant/DecalerActivity.kt
@@ -0,0 +1,10 @@
+package com.example.waterplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DecalerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
