332f4872bfaec749f2ca8a2892aafae9d5c491ea
anesboz <anesbouzouaoui@gmail.com>
2022-01-09 03:47:23 +0100

    Sun Jan  9 03:47:23 CET 2022

--- app/src/main/java/com/example/waterplant/ArrosageActivity.kt
+++ app/src/main/java/com/example/waterplant/ArrosageActivity.kt
@@ -3,20 +3,39 @@ package com.example.waterplant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.waterplant.adapters.ArrosageAdapter
 import com.example.waterplant.databinding.ActivityArrosageBinding
+import com.example.waterplant.entities.Plant
+import com.example.waterplant.viewmodels.ArrosageViewModel
+import com.example.waterplant.viewmodels.MainViewModel
 
 class ArrosageActivity : AppCompatActivity() {
+    private val model by lazy { ViewModelProvider(this).get(ArrosageViewModel::class.java) }
+    private val adapter by lazy { ArrosageAdapter(this) }
     private lateinit var binding: ActivityArrosageBinding // binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityArrosageBinding.inflate(layoutInflater) // binding
         setContentView(binding.root) // binding
 
+//        model.allArrosages = MutableLiveData()
+//        model.loadArrosages()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        model.allArrosages.observe(this) {
+            adapter.setArrosages(it)
+        }
 
         binding.back.setOnClickListener {
             finish()
         }
+
     }
 
     fun back(v: View) {
--- app/src/main/java/com/example/waterplant/MainActivity.kt
+++ app/src/main/java/com/example/waterplant/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.waterplant.adapters.MainAdapter
 import com.example.waterplant.viewmodels.MainViewModel
 import com.example.waterplant.databinding.ActivityMainBinding
+import com.example.waterplant.entities.Plant
+import com.example.waterplant.room.PlantItem
 
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate( layoutInflater ) }
-    private val model  by lazy {ViewModelProvider(this).get(MainViewModel::class.java)}
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val model by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     private val adapter by lazy { MainAdapter(this) }
 
 
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
-        model.plants.observe(this){
+        model.plants.observe(this) {
             adapter.setPlants(it)
         }
 
@@ -37,9 +39,33 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun testerArrosage(v: View)  {
+    fun testerArrosage(v: View) {
         Intent(this, ArrosageActivity::class.java).also {
             startActivity(it)
         }
     }
+
+    fun initialiser(view: View) {
+        val names = resources.getStringArray(R.array.name)
+        val latinNames = resources.getStringArray(R.array.latinName)
+        val freqArosages = resources.getIntArray(R.array.freqArosage)
+        val lastArosages = resources.getStringArray(R.array.lastArosage)
+
+        val freqNutriments = resources.getIntArray(R.array.freqNutriment)
+        val lastNutriments = resources.getStringArray(R.array.lastNutriment)
+
+        for (i in names.indices) {
+            model.addPlant(
+                PlantItem(
+                    names[i],
+                    latinNames[i],
+                    freqArosages[i],
+                    lastArosages[i],
+                    freqNutriments[i],
+                    lastNutriments[i]
+                )
+            )
+        }
+    }
+
 }
